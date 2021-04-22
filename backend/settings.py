899bceb9f99230053b94d585b72aa71f0d0093e7
sarthak punjabi <sarthakpunjabi@sarthaks-MacBook-Pro.local>
2021-04-22 23:18:51 +0530

    adding buildpack

--- backend/settings.py
+++ backend/settings.py
@@ -102,7 +102,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'insensee/build')
+            os.path.join(BASE_DIR, 'insensee/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
